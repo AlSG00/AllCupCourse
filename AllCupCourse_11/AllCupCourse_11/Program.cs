ee293a96971454dca70f83dd6f87133408a72839
AlSG00 <alex.gub2013@gmail.com>
2023-04-05 00:57:30 +0700

    2 more tasks
    + miner chest
    + online-championship
    
    on the paper:
    - Cerberus

---
+++ AllCupCourse_11/AllCupCourse_11/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace AllCupCourse_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileData = File.ReadAllLines("data.txt");
+            List<string> clearedFileData = new List<string>();
+            foreach(string dataBlock in fileData)
+            {
+                if (dataBlock != "")
+                {
+                    clearedFileData.Add(dataBlock);
+                }
+            }
+
+            string[] splittedData = clearedFileData[0].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> participants = new List<string>();
+            List<string> circleTimeStringList = new List<string>();
+            List<double> circleTimeDoubleList = new List<double>();
+
+            int participantsCount = Convert.ToInt32(splittedData[0]);
+            int circlesCount = Convert.ToInt32(splittedData[1]);
+            for (int i = 1; i < participantsCount * 2 + 1; i += 2)
+            {
+                participants.Add(clearedFileData[i]);
+                circleTimeStringList.Add(clearedFileData[i + 1]);
+            }
+
+            for (int i = 0; i < circleTimeStringList.Count; i++)
+            {
+                double totalTime = CalculateTotalTime(circleTimeStringList[i]);
+                circleTimeDoubleList.Add(totalTime);
+            }
+
+            int bestScore = circleTimeDoubleList.IndexOf(circleTimeDoubleList.Min());
+            string winnerNickname = participants[bestScore];
+
+            Console.WriteLine(winnerNickname);
+            Console.ReadLine();
+        }
+
+        private static double CalculateTotalTime(string circleTimeString)
+        {
+            string[] circleTimeStringList = circleTimeString.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            double totalTime = 0;
+            foreach (string cirleTime in circleTimeStringList)
+            {
+                //totalTime += Convert.ToDouble(cirleList);
+                // totalTime += float.Parse(cirleTime, CultureInfo.InvariantCulture.NumberFormat);
+                totalTime += double.Parse(cirleTime, CultureInfo.InvariantCulture.NumberFormat);
+            }
+
+            return totalTime;
+        }
+    }
+}
---
+++ AllCupCourse_12/AllCupCourse_12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllCupCourse_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int targetValue = 175801;
+
+            int chestOpenCount = 20;
+            int startValue = 0;
+            int secondValue = 0;
+            int currentValue = 0;
+            int lastValue = 0;
+            int beforeLastValue = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                startValue++;
+
+                secondValue = 0;
+                for (int j = 0; j < startValue; j++)
+                {
+                    secondValue++;
+
+                    lastValue = secondValue;
+                    beforeLastValue = startValue;
+                    currentValue = 0;
+                    while (currentValue < targetValue)
+                    {
+                        currentValue = lastValue + beforeLastValue;
+                        beforeLastValue = lastValue;
+                        lastValue = currentValue;
+                        Console.WriteLine(currentValue);
+                    }
+
+                    if (lastValue == targetValue)
+                    {
+                        break;
+                    }
+                }
+
+                if (lastValue == targetValue)
+                {
+                    Console.Write($"Answer is {startValue} {secondValue}");
+                    
+                    break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
